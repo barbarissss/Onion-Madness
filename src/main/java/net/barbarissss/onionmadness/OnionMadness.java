8af3fc807944cf5487e20b261a8f350bccc1d54e
Barbaris <miborisov11@gmail.com>
2024-03-26 10:25:03 +0300

    added onion rings (no functionality)

--- src/main/java/net/barbarissss/onionmadness/OnionMadness.java
+++ src/main/java/net/barbarissss/onionmadness/OnionMadness.java
@@ -1,5 +1,6 @@
 package net.barbarissss.onionmadness;
 
+import net.barbarissss.onionmadness.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,10 +12,6 @@ public class OnionMadness implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/barbarissss/onionmadness/item/ModItems.java
@@ -0,0 +1,32 @@
+package net.barbarissss.onionmadness.item;
+
+import net.barbarissss.onionmadness.OnionMadness;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems
+{
+    public static final Item ONION_RING = registerItem("onion_ring", new Item(new FabricItemSettings()));
+
+    private static void addItemsToItemGroup(FabricItemGroupEntries entries)
+    {
+        entries.add(ONION_RING);
+    }
+
+    private static Item registerItem(String name, Item item)
+    {
+        return Registry.register(Registries.ITEM, new Identifier(OnionMadness.MOD_ID, name), item);
+    }
+
+    public static void registerModItems()
+    {
+        OnionMadness.LOGGER.info("reg item for" + OnionMadness.MOD_ID);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(ModItems::addItemsToItemGroup);
+    }
+}
